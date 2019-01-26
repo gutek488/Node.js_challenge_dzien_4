0dfc724c5e07a8ab091e762a845b6ae1ce0e5c3e
Gustaw <gutek488@gmail.com>
2019-01-26 13:54:32 +0100

    Ukończono dzień 4

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.end("Hello World from back-end!");
+
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,12 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const userAgent = req.headers['user-agent'];
+    res.setHeader("Content-Type", "text/html");
+    res.end(`${userAgent}`);
+
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
